4d6c3a9145c048ed41f344720471c8b2d3375e08
iyungui <rsdbddml@gmail.com>
2024-06-25 23:21:21 +0900

    Initialize project WisdomWeather

---
+++ WisdomWeather/WisdomWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WisdomWeatherApp.swift
+//  WisdomWeather
+//
+//  Created by Yungui Lee on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct WisdomWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
