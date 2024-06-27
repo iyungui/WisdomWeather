f0f68e69f11bb13e8eb7df9203a6e67d9d47a5a5
iyungui <rsdbddml@gmail.com>
2024-06-27 10:33:56 +0900

    Add Widget

---
+++ Widget/WidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetBundle.swift
+//  Widget
+//
+//  Created by Yungui Lee on 6/27/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetBundle: SwiftUI.WidgetBundle {
+    var body: some SwiftUI.Widget {
+        Widget()
+    }
+}
